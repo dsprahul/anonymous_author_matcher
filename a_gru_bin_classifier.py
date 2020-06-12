8f7e7dd82c6c5cf584c849976f4d5d89ca96dbb6
RahulDamineni <daminenirahul@gmail.com>
2020-06-11 20:39:13 -0700

    Fine tuning, persisting hidden states & model, logging accuracy & error

--- a_gru_bin_classifier.py
+++ a_gru_bin_classifier.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.nn as nn
 import numpy as np
@@ -10,15 +11,20 @@ from functools import reduce
 from redditors_comments_dataset import RedditComments
 
 
+parser = argparse.ArgumentParser(description="Trains a GRU binary classifer")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+parser.add_argument("-n", "--name")
+args = parser.parse_args()
 
-EXPT_NAME = "default"
+EXPT_NAME = args.name or "default"
 BATCH_SIZE = 1
 EPOCHS = 10
 LEARNING_RATE = 0.0001
 HIDDEN_REPR_DIM = 50
 MODEL_PERSIST_PATH = f'./{EXPT_NAME}_model.weights'
+
+print(f'Running {EXPT_NAME}...')
 
 
 class BinaryClassifier(nn.Module):
@@ -29,7 +35,7 @@ class BinaryClassifier(nn.Module):
         self.embedding_size = embedding_size
         self.gru = nn.GRU(embedding_size, hidden_size)
         self.fc = nn.Linear(hidden_size, 2)
-        self.sm = nn.Softmax(dim=0)
+        self.sm = nn.Softmax(dim=2)
 
     def forward(self, input, hidden):
 
@@ -41,20 +47,20 @@ class BinaryClassifier(nn.Module):
         # output = embedded_sequence
         output, hidden = self.gru(input, hidden)
 
-        self.representation = hidden
+        # self.representation = hidden
 
         fc_out = self.fc(hidden)
         sm_out = self.sm(fc_out)
 
-        return sm_out
+        return sm_out, hidden.data.cpu().numpy()
 
     def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+        return np.zeros((1, 1, self.hidden_size))
 
 
 if __name__ == "__main__":
     writer = SummaryWriter(log_dir=f'./log/{EXPT_NAME}')
-
+    torch.autograd.set_detect_anomaly(True)
     classifier = BinaryClassifier(
         hidden_size=HIDDEN_REPR_DIM,
         embedding_size=300
@@ -62,7 +68,7 @@ if __name__ == "__main__":
 
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(classifier.parameters(), lr=LEARNING_RATE)
-    inital_hidden = classifier.initHidden()
+    hidden = classifier.initHidden()  # Initialize hidden state
     classifier.train()
 
     train = RedditComments(path_to_json="./data/anon_auth_dataset.json")
@@ -73,15 +79,19 @@ if __name__ == "__main__":
 
     for e in range(1, EPOCHS + 1):
         print(f'\nTraining Epoch#{e}')
+        train_accu, test_accu = 0, 0
 
         for X, y in tqdm(train_loader):
 
             y = torch.Tensor([0., 1.]) if y == 1 else torch.Tensor([1., 0.])
             y = y.view(1, 1, -1).float().to(device)
-            y_hat = classifier.forward(input=X.to(device), hidden=inital_hidden)
-
+            y_hat, hidden = classifier.forward(
+                input=X.to(device),
+                hidden=torch.Tensor(hidden).to(device)
+            )
             optimizer.zero_grad()
             train_loss = criterion(y_hat, y)
+            train_accu += (torch.argmax(y_hat) == torch.argmax(y)).item()
             train_loss.backward()
             optimizer.step()
 
@@ -92,20 +102,28 @@ if __name__ == "__main__":
             for eval_X, eval_y in eval_loader:
 
                 eval_y = torch.Tensor([0., 1.]) if eval_y == 1 else torch.Tensor([1., 0.])
-                eval_y = eval_y.view(1, 1, -1).float()
-                pred_y = classifier.forward(
+                eval_y = eval_y.view(1, 1, -1).float().to(device)
+                pred_y, _ = classifier.forward(
                     input=eval_X.to(device),
-                    hidden=inital_hidden
+                    hidden=torch.Tensor(hidden).to(device)
                 )
 
                 test_loss = criterion(pred_y, eval_y)
                 agg_test_loss += test_loss.item()
+                test_accu += (torch.argmax(pred_y) == torch.argmax(eval_y)).item()
 
             agg_test_loss /= len(eval)
+            test_accu /= len(eval)
+            train_accu /= len(train)
 
             writer.add_scalars("LSTM_Classifier/loss", {
                 "test": agg_test_loss,
                 "train": train_loss.item()
+            }, e)
+
+            writer.add_scalars("LSTM_Classifier/accuracy", {
+                "test": test_accu,
+                "train": train_accu
             }, e)
 
             torch.save(classifier.state_dict(), MODEL_PERSIST_PATH)
