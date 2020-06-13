80a430af3d71ead830123d625b6ba6673b6a6ea4
RahulDamineni <daminenirahul@gmail.com>
2020-06-12 19:57:20 -0700

    inference pipeline and example

--- dbms.py
+++ dbms.py
@@ -1,22 +1,54 @@
+import json
 import torch
-import a_gru_bin_classifier
 import torchtext
+import fasttext
+import numpy as np
 from torchtext.data import get_tokenizer
+from a_gru_bin_classifier import (
+    BinaryClassifier,
+    HIDDEN_REPR_DIM
+)
 from uuid import uuid4
+from tqdm import tqdm
 
-ModelClass = a_gru_bin_classifier.BinaryClassifier
-MODEL_WEIGHTS = a_gru_bin_classifier.MODEL_PERSIST_PATH
-HIDDEN_REPR_DIM = a_gru_bin_classifier.HIDDEN_REPR_DIM
+
+ModelClass = BinaryClassifier
+DATASET_PATH = "./data/anon_auth_dataset.json"
+MODEL_PERSIST_PATH = "./models/gru_v0.7_2.5k_model.weights"
 SEP = "<SEP>"
 
-model = ModelClass(hidden_size=HIDDEN_REPR_DIM)
-model.load_state_dict(torch.load(MODEL_WEIGHTS))
 
-model.eval()
+def get_text_representation(text):
+    tokens = tokenizer(text)
+    input = tokens + [SEP] + tokens
+    input_embeddings = torch.Tensor([[
+        embedding.get_word_vector(token)
+        for token in input
+    ]])
+    _, representation = model.forward(
+        input=input_embeddings.unsqueeze(0),
+        hidden=torch.Tensor(model.initHidden())
+    )
 
-tokenizer = get_tokenizer("spacy")
+    return representation
 
-AUTHORS_DB = {}
+
+def make_authors_db(model, train_df):
+    AUTHORS_DB = {}
+    UUID_AUTHORS = {}
+
+    for author, comments in tqdm(train_df.items()):
+
+        representation = np.zeros((len(comments), HIDDEN_REPR_DIM))
+        for cid, comment in enumerate(comments):
+            comments[cid] = get_text_representation(text=comment)
+        representation = representation.mean(axis=0)
+
+        author_uuid = uuid4()
+        AUTHORS_DB[author_uuid] = representation
+        UUID_AUTHORS[author_uuid] = author
+
+    return AUTHORS_DB, UUID_AUTHORS
 
 
 def find_author_id(query, threshold=0.9):
@@ -27,14 +59,6 @@ def find_author_id(query, threshold=0.9):
     return uuid4()  # Return new ID
 
 
-def get_text_representation(text):
-    tokens = tokenizer(text)
-    input = tokens + [SEP] + tokens
-    model.forward(input=input, hidden=model.initHidden())
-
-    return model.representation
-
-
 def determine_author(text):
 
     representation = get_text_representation(text)
@@ -43,30 +67,53 @@ def determine_author(text):
     if author_id in AUTHORS_DB:
         return {
             "id": author_id,
+            "name": UUID_AUTHORS[author_id],
             "meta": "existing_author"
         }
     else:
         return {
             "id": author_id,
+            "name": "<new_author>",
             "meta": "new_author"
         }
 
 
+def evaluate(df, comments_per_author=20):
+    accuracy = 0
+    total_comments = len(df) * comments_per_author
+
+    for author, comments in df.items():
+        for comment in comments:
+            predicted_author = determine_author(text=comment)
+            accuracy += predicted_author == author
+
+    return (100 * accuracy) / total_comments
+
+
 if __name__ == "__main__":
-    data_point = {
-        "username": "bobdeb12",
-        "source": "facebook",
-        "body": "blah blah blah.."
-    }
-    data_point2 = {
-        "username": "debbob12",
-        "source": "quora",
-        "body": "blah blah blah.."  # Similar writing style as <bobdeb12>
-    }
+
+    with open(DATASET_PATH) as in_:
+        train_df, _, test_df = json.load(in_)
+        print(f'training on {len(train_df)} authors...')
+
+    model = ModelClass(hidden_size=HIDDEN_REPR_DIM)
+    model.load_state_dict(
+        torch.load(MODEL_PERSIST_PATH, map_location="cpu"),
+        strict=False
+    )
+
+    print(f'Loading tokenizer & embedding model... ', end=' ')
+    tokenizer = get_tokenizer("spacy")
+    embedding = fasttext.load_model("wiki.simple/wiki.simple.bin")
+    print(f'done.')
+
+    print(f'Calculating authors train representations... ')
+    AUTHORS_DB, UUID_AUTHORS = make_authors_db(
+        model=model,
+        train_df=train_df
+    )
 
     # Do magic here
-    data_point["author"] = determine_author(data_point["body"])
-
-    db.collection.insert(
-        data_point
-    )
+    train_accu = evaluate(df=train_df, comments_per_author=60)
+    test_accu = evaluate(df=test_df, comments_per_author=20)
+    print(f'Train accuracy: {train_accu:.2f}%, Test accuracy: {test_accu:.2f}%')
